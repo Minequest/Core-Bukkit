dddd9d7b89d8373aa5d051d62a2edc60dbe3d353
Alan Campbell <alan.echobob@gmail.com>
2012-03-04 00:15:55 -0500

    Added a small bit of code for signs. And added some methods for getting plugin name.

--- src/main/java/com/theminequest/MineQuest/MineQuest.java
+++ src/main/java/com/theminequest/MineQuest/MineQuest.java
@@ -1,5 +1,6 @@
 package com.theminequest.MineQuest;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MineQuest extends JavaPlugin {
@@ -9,10 +10,19 @@ public class MineQuest extends JavaPlugin {
 	 * But we could use Bukkit.getPluginManager().getPlugin("MineQuest")...
 	 */
 	public static MineQuest activePlugin = null;
-	
+    private static PluginDescriptionFile description;
+    
+    public static String getVersion() {
+        return description.getVersion();
+    }
+
+    public static String getPluginName() {
+        return description.getName();
+    }
 	@Override
 	public void onEnable(){
 		activePlugin = this;
+
 	}
 
 	
