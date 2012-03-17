deda6603541d78e8e8de44508ef8a22d6de8a396
Alan Campbell <echobob@theminequest.com>
2012-03-16 21:55:26 -0400

    Sql Related Classes. Needs work.

---
+++ src/main/java/SqlCore/EditSQL.java
@@ -0,0 +1,8 @@
+package SqlCore;
+
+public class EditSQL {
+	public boolean add(String table, String value1, String value2, String value3, String value4){ 
+		//Todo add to table
+		return false;
+	}
+}
