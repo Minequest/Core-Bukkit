624791c56e712687805c162e1f9d3152278997be
Robert Xu <robxu9@gmail.com>
2012-03-17 23:49:46 -0400

    Implemented now in last commit.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MineQuest/SQLCore/EditSQL.java
+++
@@ -1,8 +0,0 @@
-package com.theminequest.MineQuest.SQLCore;
-
-public class EditSQL {
-	public boolean add(String table, String value1, String value2, String value3, String value4){ 
-		//Todo add to table
-		return false;
-	}
-}
--- src/main/java/com/theminequest/MineQuest/SQLCore/UpdateSql.java
+++
@@ -1,5 +0,0 @@
-package com.theminequest.MineQuest.SQLCore;
-
-public class UpdateSql {
-//When we need to change the Sql file like if we make changes to the format. 
-}
