1633c5e5587497457c5392527531a7deb6144070
Robert Xu <robxu9@gmail.com>
2012-03-23 22:57:45 -0400

    Move that ARSE-ache QuestParser to its own class + static method.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MineQuest/Quest/Quest.java
+++ src/main/java/com/theminequest/MineQuest/Quest/Quest.java
@@ -59,36 +59,36 @@ import com.theminequest.MineQuest.Team.Team;
 
 public class Quest {
 
-	private Team team;
-	private String questname;
-	private long questid;
-	private boolean started;
-	private int currenttask;
+	protected Team team;
+	protected String questname;
+	protected long questid;
+	protected boolean started;
+	protected int currenttask;
 
 	// always <ID #,OBJECT/DETAILS>
 	// TreeMap guarantees key order.
 	// (yes, treemap is RESOURCE intensive D:,
 	// but I have to combine it with LinkedHashMap to ensure there
 	// will be no duplicates)
-	private TreeMap<Integer, Task> tasks;
-	private TreeMap<Integer, String> events;
-	private TreeMap<Integer, TargetDetails> targets;
-	private TreeMap<Integer, Edit> editables;
+	protected TreeMap<Integer, Task> tasks;
+	protected TreeMap<Integer, String> events;
+	protected TreeMap<Integer, TargetDetails> targets;
+	protected TreeMap<Integer, Edit> editables;
 	// quest configuration
-	private String displayname;
-	private boolean questRepeatable;
-	private boolean spawnReset;
+	protected String displayname;
+	protected boolean questRepeatable;
+	protected boolean spawnReset;
 	/**
 	 * Controls the Spawn Point for the Quest (x,y,z)
 	 */
-	private double[] spawnPoint;
+	protected double[] spawnPoint;
 	/**
 	 * Controls the area to preserve (uneditable) (x,y,z,x,y,z)
 	 */
-	private double[] areaPreserve;
-	private String editMessage;
-	private String world;
-	private boolean loadworld;
+	protected double[] areaPreserve;
+	protected String editMessage;
+	protected String world;
+	protected boolean loadworld;
 
 	protected Quest(long questid, String id, Team t) {
 		team = t;
@@ -120,7 +120,7 @@ public class Quest {
 
 		// DEFAULTS end
 		try {
-			parseDefinition();
+			QuestParser.parseDefinition(this);
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
@@ -139,133 +139,6 @@ public class Quest {
 				throw new RuntimeException(e);
 			}
 		}
-	}
-
-	/*
-	 * Name:dsafasdf Repeatable:true Reset:true Spawn:0:64:0
-	 * AreaPreserve:10:10:10:30:30:30 EditMessage:Eeek! No editing!
-	 * NPC:&3Echobob:0:64:0:0:0 NPCV:&4DrEeeevil:5:64:5:0:0 World:fadsf
-	 * LoadWorld:: Instance::: QuestArea:::::: Event:3:T:StartQuest::5:
-	 * Event:2:T:NPCPropertyEvent::: Target:132:NPCTarget:&3Echobob,
-	 * Edit::CanEdit:::: Task:0: RepeatingTask:0:
-	 * DisallowedAbilities:Ability,Ability2,Ability3
-	 */
-
-	private void parseDefinition() throws FileNotFoundException {
-		LinkedHashMap<Integer, Task> tasks = new LinkedHashMap<Integer, Task>();
-		LinkedHashMap<Integer, String> events = new LinkedHashMap<Integer, String>();
-		LinkedHashMap<Integer, TargetDetails> targets = new LinkedHashMap<Integer, TargetDetails>();
-		LinkedHashMap<Integer, Edit> editables = new LinkedHashMap<Integer,Edit>();
-		File f = new File(MineQuest.activePlugin.getDataFolder()
-				+ File.separator + "quests" + File.separator + questname
-				+ ".quest");
-		Scanner filereader = new Scanner(f);
-		while (filereader.hasNextLine()) {
-			String nextline = filereader.nextLine();
-			ArrayList<String> ar = (ArrayList<String>) Arrays.asList(nextline
-					.split(":"));
-			String type = ar.get(0).toLowerCase();
-			if (type.equals("name"))
-				displayname = ar.get(1);
-			else if (type.equals("repeatable"))
-				questRepeatable = (ar.get(1).equals("true"));
-			else if (type.equals("reset"))
-				spawnReset = (ar.get(1).equals("true"));
-			else if (type.equals("spawn")) {
-				if (!ar.get(1).equals(""))
-					spawnPoint[0] = Double.parseDouble(ar.get(1));
-				if (!ar.get(2).equals(""))
-					spawnPoint[1] = Double.parseDouble(ar.get(2));
-				if (!ar.get(3).equals(""))
-					spawnPoint[2] = Double.parseDouble(ar.get(3));
-			} else if (type.equals("areapreserve")) {
-				if (!ar.get(1).equals(""))
-					areaPreserve[0] = Double.parseDouble(ar.get(1));
-				if (!ar.get(2).equals(""))
-					areaPreserve[1] = Double.parseDouble(ar.get(2));
-				if (!ar.get(3).equals(""))
-					areaPreserve[2] = Double.parseDouble(ar.get(3));
-				if (!ar.get(4).equals(""))
-					areaPreserve[3] = Double.parseDouble(ar.get(4));
-				if (!ar.get(5).equals(""))
-					areaPreserve[4] = Double.parseDouble(ar.get(5));
-				if (!ar.get(6).equals(""))
-					areaPreserve[5] = Double.parseDouble(ar.get(6));
-			} else if (type.equals("editmessage"))
-				editMessage = ChatColor.GRAY + ar.get(1);
-			else if (type.equals("world"))
-				world = ar.get(1);
-			else if (type.equals("loadworld")) {
-				// I say YES to instances.
-				loadworld = true;
-				world = ar.get(2);
-				// I do NOT care about QuestArea, because
-				// I simply delete the world when done.
-			} else if (type.equals("event")) {
-				int number = Integer.parseInt(ar.get(1));
-				// T = targeted event
-				boolean targetedevent = false;
-				if (ar.get(2).equals("T")) {
-					ar.remove(2);
-					targetedevent = true;
-				}
-				String eventname = ar.get(2);
-				String details = "";
-				if (targetedevent)
-					details += "T:";
-				for (int i = 3; i < ar.size(); i++) {
-					details += ar.get(i);
-					if (i < ar.size() - 1) {
-						details += ":";
-					}
-				}
-				// final result: "eventname:T:details"
-				events.put(number, eventname + ":" + details);
-			} else if (type.equals("target")) {
-				int number = Integer.parseInt(ar.get(1));
-				String d = "";
-				for (int i=2; i<ar.size(); i++){
-					d += ar.get(i);
-					if (i!=ar.size()-1)
-						d+=":";
-				}
-				targets.put(number, new TargetDetails(questid,d));
-			} else if (type.equals("edit")) {
-				int number = Integer.parseInt(ar.get(1));
-				String edittype = ar.get(2);
-				String d = "";
-				for (int i=3; i<ar.size(); i++){
-					d += ar.get(i);
-					if (i!=ar.size()-1)
-						d+=":";
-				}
-				Edit e;
-				if (edittype.equalsIgnoreCase("CanEdit"))
-					e = new CoordinateEdit(questid,number,Integer.parseInt(d.split(":")[3]),d);
-				else if (edittype.equalsIgnoreCase("CanEditArea"))
-					e = new InsideAreaEdit(questid,number,Integer.parseInt(d.split(":")[6]),d);
-				else if (edittype.equalsIgnoreCase("CanEditOutsideArea"))
-					e = new OutsideAreaEdit(questid,number,Integer.parseInt(d.split(":")[6]),d);
-				else {
-					int taskid = Integer.parseInt(ar.get(3));
-					d = "";
-					for (int i=4; i<ar.size(); i++){
-						d += ar.get(i);
-						if (i!=ar.size()-1)
-							d+=":";
-					}
-					if (edittype.equalsIgnoreCase("CanEditTypesInHand"))
-						e = new ItemInHandEdit(questid,number,taskid,d);
-					else
-						e = new CertainBlockEdit(questid,number,taskid,d);
-				}
-				editables.put(number, e);
-			}
-		}
-		this.tasks = new TreeMap<Integer, Task>(tasks);
-		this.events = new TreeMap<Integer, String>(events);
-		this.targets = new TreeMap<Integer, TargetDetails>(targets);
-		this.editables = new TreeMap<Integer, Edit>(editables);
 	}
 
 	/**
---
+++ src/main/java/com/theminequest/MineQuest/Quest/QuestParser.java
@@ -0,0 +1,142 @@
+package com.theminequest.MineQuest.Quest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import org.bukkit.ChatColor;
+
+import com.theminequest.MineQuest.MineQuest;
+import com.theminequest.MineQuest.Editable.CertainBlockEdit;
+import com.theminequest.MineQuest.Editable.CoordinateEdit;
+import com.theminequest.MineQuest.Editable.Edit;
+import com.theminequest.MineQuest.Editable.InsideAreaEdit;
+import com.theminequest.MineQuest.Editable.ItemInHandEdit;
+import com.theminequest.MineQuest.Editable.OutsideAreaEdit;
+import com.theminequest.MineQuest.Target.TargetDetails;
+import com.theminequest.MineQuest.Tasks.Task;
+
+public class QuestParser {
+	
+	protected static void parseDefinition(Quest q) throws FileNotFoundException{
+		LinkedHashMap<Integer, Task> tasks = new LinkedHashMap<Integer, Task>();
+		LinkedHashMap<Integer, String> events = new LinkedHashMap<Integer, String>();
+		LinkedHashMap<Integer, TargetDetails> targets = new LinkedHashMap<Integer, TargetDetails>();
+		LinkedHashMap<Integer, Edit> editables = new LinkedHashMap<Integer,Edit>();
+		File f = new File(MineQuest.activePlugin.getDataFolder()
+				+ File.separator + "quests" + File.separator + q.questname
+				+ ".quest");
+		Scanner filereader = new Scanner(f);
+		while (filereader.hasNextLine()) {
+			String nextline = filereader.nextLine();
+			ArrayList<String> ar = (ArrayList<String>) Arrays.asList(nextline
+					.split(":"));
+			String type = ar.get(0).toLowerCase();
+			if (type.equals("name"))
+				q.displayname = ar.get(1);
+			else if (type.equals("repeatable"))
+				q.questRepeatable = (ar.get(1).equals("true"));
+			else if (type.equals("reset"))
+				q.spawnReset = (ar.get(1).equals("true"));
+			else if (type.equals("spawn")) {
+				if (!ar.get(1).equals(""))
+					q.spawnPoint[0] = Double.parseDouble(ar.get(1));
+				if (!ar.get(2).equals(""))
+					q.spawnPoint[1] = Double.parseDouble(ar.get(2));
+				if (!ar.get(3).equals(""))
+					q.spawnPoint[2] = Double.parseDouble(ar.get(3));
+			} else if (type.equals("areapreserve")) {
+				if (!ar.get(1).equals(""))
+					q.areaPreserve[0] = Double.parseDouble(ar.get(1));
+				if (!ar.get(2).equals(""))
+					q.areaPreserve[1] = Double.parseDouble(ar.get(2));
+				if (!ar.get(3).equals(""))
+					q.areaPreserve[2] = Double.parseDouble(ar.get(3));
+				if (!ar.get(4).equals(""))
+					q.areaPreserve[3] = Double.parseDouble(ar.get(4));
+				if (!ar.get(5).equals(""))
+					q.areaPreserve[4] = Double.parseDouble(ar.get(5));
+				if (!ar.get(6).equals(""))
+					q.areaPreserve[5] = Double.parseDouble(ar.get(6));
+			} else if (type.equals("editmessage"))
+				q.editMessage = ChatColor.GRAY + ar.get(1);
+			else if (type.equals("world"))
+				q.world = ar.get(1);
+			else if (type.equals("loadworld")) {
+				// I say YES to instances.
+				q.loadworld = true;
+				q.world = ar.get(2);
+				// I do NOT care about QuestArea, because
+				// I simply delete the world when done.
+			} else if (type.equals("event")) {
+				int number = Integer.parseInt(ar.get(1));
+				// T = targeted event
+				boolean targetedevent = false;
+				if (ar.get(2).equals("T")) {
+					ar.remove(2);
+					targetedevent = true;
+				}
+				String eventname = ar.get(2);
+				String details = "";
+				if (targetedevent)
+					details += "T:";
+				for (int i = 3; i < ar.size(); i++) {
+					details += ar.get(i);
+					if (i < ar.size() - 1) {
+						details += ":";
+					}
+				}
+				// final result: "eventname:T:details"
+				events.put(number, eventname + ":" + details);
+			} else if (type.equals("target")) {
+				int number = Integer.parseInt(ar.get(1));
+				String d = "";
+				for (int i=2; i<ar.size(); i++){
+					d += ar.get(i);
+					if (i!=ar.size()-1)
+						d+=":";
+				}
+				targets.put(number, new TargetDetails(q.questid,d));
+			} else if (type.equals("edit")) {
+				int number = Integer.parseInt(ar.get(1));
+				String edittype = ar.get(2);
+				String d = "";
+				for (int i=3; i<ar.size(); i++){
+					d += ar.get(i);
+					if (i!=ar.size()-1)
+						d+=":";
+				}
+				Edit e;
+				if (edittype.equalsIgnoreCase("CanEdit"))
+					e = new CoordinateEdit(q.questid,number,Integer.parseInt(d.split(":")[3]),d);
+				else if (edittype.equalsIgnoreCase("CanEditArea"))
+					e = new InsideAreaEdit(q.questid,number,Integer.parseInt(d.split(":")[6]),d);
+				else if (edittype.equalsIgnoreCase("CanEditOutsideArea"))
+					e = new OutsideAreaEdit(q.questid,number,Integer.parseInt(d.split(":")[6]),d);
+				else {
+					int taskid = Integer.parseInt(ar.get(3));
+					d = "";
+					for (int i=4; i<ar.size(); i++){
+						d += ar.get(i);
+						if (i!=ar.size()-1)
+							d+=":";
+					}
+					if (edittype.equalsIgnoreCase("CanEditTypesInHand"))
+						e = new ItemInHandEdit(q.questid,number,taskid,d);
+					else
+						e = new CertainBlockEdit(q.questid,number,taskid,d);
+				}
+				editables.put(number, e);
+			}
+		}
+		q.tasks = new TreeMap<Integer, Task>(tasks);
+		q.events = new TreeMap<Integer, String>(events);
+		q.targets = new TreeMap<Integer, TargetDetails>(targets);
+		q.editables = new TreeMap<Integer, Edit>(editables);
+	}
+
+}
