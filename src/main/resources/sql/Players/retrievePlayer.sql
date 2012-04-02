77e79c029ab43b5b4c657383729b114109a55e78
Robert Xu <robxu9@gmail.com>
2012-04-02 14:54:20 -0400

    Fix scripts for MySQL.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/resources/sql/Players/retrievePlayer.sql
+++ src/main/resources/sql/Players/retrievePlayer.sql
@@ -1,3 +1,3 @@
 # Retrieve a player from SQL
 # %s = Player Name
-SELECT * FROM mq_player WHERE P_NAME IS '%s';
+SELECT * FROM mq_player WHERE (P_NAME='%s');
--- src/main/resources/sql/Quests/getPlayerQuestsAvailable.sql
+++ src/main/resources/sql/Quests/getPlayerQuestsAvailable.sql
@@ -1,3 +1,3 @@
 # Get all quests available for this player
 # %s - player name
-SELECT * FROM MQ_QUEST WHERE P_NAME = '%s' AND ISCOMPLETED = 0
+SELECT * FROM MQ_QUEST WHERE (P_NAME = '%s') AND (ISCOMPLETED = 0);
