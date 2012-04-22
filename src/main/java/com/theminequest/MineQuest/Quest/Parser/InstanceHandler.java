8afb835eb693925387e4390a64f22d8ed0c8f6e4
Robert Xu <robxu9@gmail.com>
2012-04-22 15:03:10 -0400

    Fix InstanceHandler.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MineQuest/Quest/Parser/InstanceHandler.java
+++ src/main/java/com/theminequest/MineQuest/Quest/Parser/InstanceHandler.java
@@ -1,6 +1,20 @@
 package com.theminequest.MineQuest.Quest.Parser;
 
+import java.util.List;
+
+import com.theminequest.MineQuest.Quest.QuestDescription;
+import com.theminequest.MineQuest.Quest.QuestParser.QHandler;
+
 @Deprecated
-public class InstanceHandler extends LoadWorldHandler {
+public class InstanceHandler implements QHandler {
+
+	@Override
+	public void parseDetails(QuestDescription q, List<String> line) {
+			// I say YES to instances.
+			q.loadworld = true;
+			q.world = line.get(2);
+			if (line.size()>3)
+				q.nether = true;
+	}
 
 }
