03554f062367e1dff44fa92aa69fb7c852f9cd26
Robert Xu <robxu9@gmail.com>
2012-06-13 17:07:49 -0400

    I *may* have gotten lazy with RequirementFactory >.>
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MineQuest/Quest/RequirementFactory.java
+++ src/main/java/com/theminequest/MineQuest/Quest/RequirementFactory.java
@@ -7,8 +7,9 @@ import com.theminequest.MineQuest.API.Quest.QuestRequirement.Type;
 public class RequirementFactory {
 	
 	public static QuestRequirement constructRequirement(String type, QuestDetails quest, String details){
-		if (type.equalsIgnoreCase("neverdone"))
-			return new QuestReq(Type.NEVERDONE,quest,details);
+		Type t = Type.valueOf(type.toUpperCase());
+		if (t!=null)
+			return new QuestReq(t,quest,details);
 		return null;
 	}
 
