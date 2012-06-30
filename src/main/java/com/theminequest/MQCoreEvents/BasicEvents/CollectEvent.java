f3ff34f13030c5a0ff62081e0a742e1fe3313e0c
Zak Ford <zak.j.ford@gmail.com>
2012-06-29 20:41:15 -0400

    Rewrote CollectEvent

--- src/main/java/com/theminequest/MQCoreEvents/BasicEvents/CollectEvent.java
+++ src/main/java/com/theminequest/MQCoreEvents/BasicEvents/CollectEvent.java
@@ -19,27 +19,30 @@
  **/
 package com.theminequest.MQCoreEvents.BasicEvents;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-import com.theminequest.MineQuest.MineQuest;
 import com.theminequest.MineQuest.API.CompleteStatus;
 import com.theminequest.MineQuest.API.Managers;
 import com.theminequest.MineQuest.API.Events.UserQuestEvent;
 import com.theminequest.MineQuest.API.Events.QuestEvent;
-import com.theminequest.MineQuest.API.Group.Group;
-import com.theminequest.MineQuest.API.Group.QuestGroup;
-import com.theminequest.MineQuest.Group.Party;
+import com.theminequest.MineQuest.API.Utils.InventoryUtils;
 
 public class CollectEvent extends QuestEvent implements UserQuestEvent {
 
 	private int taskid;
-	private List<Integer> itemids;
-	private int totaltocollect;
+	private Map<Material, Integer> itemMap;
+	private Future<Boolean> futureTask;
 
 	/*
 	 * (non-Javadoc)
@@ -52,12 +55,39 @@ public class CollectEvent extends QuestEvent implements UserQuestEvent {
 	@Override
 	public void parseDetails(String[] details) {
 		taskid = Integer.parseInt(details[0]);
+		itemMap = Collections.synchronizedMap(new LinkedHashMap<Material, Integer>());
 		String[] items = details[1].split(",");
-		itemids = new ArrayList<Integer>();
-		for (String i : items){
-			itemids.add(Integer.parseInt(i));
+		String[] amounts = details[2].split(",");
+		
+		for (int i = 0; i < items.length; i++) {
+			String item = items[i];
+			Integer amount = null;
+			try {
+				if (amounts.length == 1) {
+						amount = Integer.valueOf(amounts[0]);
+				} else if (i < amounts.length) {
+					amount = Integer.valueOf(amounts[i]);
+				}
+			} catch (NumberFormatException e) {}
+			
+			if (amount == null) {
+				Managers.log(Level.SEVERE, "[Event] In CollectEvent, could not determine amount of items to collect for "+item);
+				continue;
+			}
+			
+			Material m = Material.matchMaterial(item.toUpperCase());
+			if (m == null) {
+				try {
+					m = Material.getMaterial(Integer.valueOf(item));
+				} catch (NumberFormatException e) {}
+			}
+			
+			if (m == null) {
+				Managers.log(Level.SEVERE, "[Event] In CollectEvent, could not determine material of "+item);
+				continue;
+			}
+			itemMap.put(m, amount);
 		}
-		totaltocollect = Integer.parseInt(details[2]);
 	}
 
 	/*
@@ -66,19 +96,47 @@ public class CollectEvent extends QuestEvent implements UserQuestEvent {
 	 */
 	@Override
 	public boolean conditions() {
-		QuestGroup g = Managers.getQuestGroupManager().get(getQuest());
-		int total = 0;
-		for (Player p : g.getMembers()){
-			PlayerInventory i = p.getInventory();
-			for (int item : itemids){
-				if (i.contains(item)){
-					total+=i.getItem(item).getAmount();
+		if (futureTask != null) {
+			if (futureTask.isDone()) {
+				Boolean ret = null;
+				try {
+					ret = futureTask.get();
+				} catch (Throwable t) {
+					t.printStackTrace();
 				}
+				futureTask = null;
+				if (ret != null && ret.booleanValue())
+					return true;
 			}
-		}
-		if (total<totaltocollect)
 			return false;
-		return true;
+		}
+		
+		Callable<Boolean> c = new Callable<Boolean>() {
+			public Boolean call() {
+				Player p = Bukkit.getServer().getPlayerExact(getQuest().getQuestOwner());
+				if (p == null)
+					return false;
+				
+				PlayerInventory i = p.getInventory();
+				for (Map.Entry<Material, Integer> entry : itemMap.entrySet()) {
+					Material m = entry.getKey();
+					int amount = entry.getValue();
+					if (!InventoryUtils.inventoryContains(i, m, 0, amount))
+						return false;
+				}
+				
+				for (Map.Entry<Material, Integer> entry : itemMap.entrySet()) {
+					ItemStack stack = new ItemStack(entry.getKey(), entry.getValue());
+					i.removeItem(stack);
+				}
+				
+				return true;
+			}
+		};
+		
+		futureTask = Bukkit.getScheduler().callSyncMethod(Bukkit.getPluginManager().getPlugin("MineQuest"), c);
+		
+		return false;
 	}
 
 	@Override
@@ -93,16 +151,25 @@ public class CollectEvent extends QuestEvent implements UserQuestEvent {
 
 	@Override
 	public String getDescription() {
-		String tr = "Collect some ";
-		for (int i=0; i<itemids.size(); i++){
-			tr+=Material.getMaterial(itemids.get(i)).toString();
-			if (i<itemids.size()-1)
-				tr+=", ";
-			else
-				tr+=", and ";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Collect ");
+		boolean first = false;
+		int i = 0;
+		for (Map.Entry<Material, Integer> entry : itemMap.entrySet()) {
+			i++;
+			if (first) {
+				first = false;
+			} else {
+				builder.append(", ");
+				
+				if (i == itemMap.size())
+					builder.append("and ");
+			}
+			
+			builder.append(entry.getValue().toString()).append(" ").append(entry.getKey().toString());
 		}
-		tr+="!";
-		return tr;
+		builder.append("!");
+		return builder.toString();
 	}
 
 }
