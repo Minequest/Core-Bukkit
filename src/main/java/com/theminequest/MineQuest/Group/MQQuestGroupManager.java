be9fbb61988c7c2daf7f1dbbc6f68c78f43b115d
Robert Xu <robxu9@gmail.com>
2012-07-10 19:31:41 -0400

    We're GPLv3.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MineQuest/Group/MQQuestGroupManager.java
+++ src/main/java/com/theminequest/MineQuest/Group/MQQuestGroupManager.java
@@ -1,22 +1,21 @@
-/**
- * This file, MQQuestGroupManager.java, is part of MineQuest:
- * A full featured and customizable quest/mission system.
- * Copyright (C) 2012 The MineQuest Party
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
+/*
+ * This file is part of MineQuest, The ultimate MMORPG plugin!.
+ * MineQuest is licensed under GNU General Public License v3.
+ * Copyright (C) 2012 The MineQuest Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- **/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.theminequest.MineQuest.Group;
 
 import java.util.ArrayList;
