0fad842439a18731b520b6382ee5a2f509acc001
Zak Ford <zak.j.ford@gmail.com>
2012-12-12 13:27:29 -0500

    Updated RewardEnchanted to reflect actual enchant numbers in minecraft

--- src/main/java/com/theminequest/MQCoreEvents/RewardEnchanted.java
+++ src/main/java/com/theminequest/MQCoreEvents/RewardEnchanted.java
@@ -18,30 +18,20 @@
  */
 package com.theminequest.MQCoreEvents;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-
-import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import com.theminequest.MineQuest.MineQuest;
 import com.theminequest.MineQuest.API.CompleteStatus;
 import com.theminequest.MineQuest.API.Managers;
 import com.theminequest.MineQuest.API.Events.QuestEvent;
-import com.theminequest.MineQuest.API.Group.Group;
 import com.theminequest.MineQuest.API.Group.QuestGroup;
 
 public class RewardEnchanted extends QuestEvent {
 	
 	private int taskid;
 	private int item;
-	private int itemEnchant;
-	private short itemDurability = 100;
-	private boolean isDurableItem;
-	private boolean isEnchantable; 
+	private short itemDurability;
 	private int enchantNumber;
 	private int enchantLevel;
 	
@@ -57,7 +47,6 @@ public class RewardEnchanted extends QuestEvent {
 		enchantNumber = Integer.parseInt(details[2]);
 		enchantLevel = Integer.parseInt(details[3]);
 		itemDurability = Short.parseShort(details[4]);
-
 	}
 
 	@Override
@@ -68,79 +57,20 @@ public class RewardEnchanted extends QuestEvent {
 	@Override
 	public CompleteStatus action() {
 		QuestGroup g = Managers.getQuestGroupManager().get(getQuest());
+		Enchantment enchantment = Enchantment.getById(enchantNumber);
+		if (enchantment == null)
+			return CompleteStatus.FAILURE;
+		
 		for (Player p : g.getMembers()){
 			
 			ItemStack itemStack = new ItemStack(item);
 			itemStack.setDurability(itemDurability);
-			itemStack.addEnchantment(enchant(enchantNumber), enchantLevel);
+			itemStack.addEnchantment(enchantment, enchantLevel);
 			
 			p.getInventory().addItem(itemStack);
 		}
 		return CompleteStatus.SUCCESS;
 	}
-	
-	public Enchantment enchant(int enchantnumber){
-		if(enchantnumber == 1){
-			return Enchantment.ARROW_DAMAGE;
-		}
-		if(enchantnumber == 2){
-			return Enchantment.ARROW_FIRE;
-		}
-		if(enchantnumber == 3){
-			return Enchantment.ARROW_KNOCKBACK;
-		}
-		if(enchantnumber == 4){
-			return Enchantment.DAMAGE_ALL;
-		}
-		if(enchantnumber == 5){
-			return Enchantment.DAMAGE_ARTHROPODS;
-		}
-		if(enchantnumber == 6){
-			return Enchantment.DAMAGE_UNDEAD;
-		}
-		if(enchantnumber == 7){
-			return Enchantment.DIG_SPEED;
-		}
-		if(enchantnumber == 8){
-			return Enchantment.DURABILITY;
-		}
-		if(enchantnumber == 9){
-			return Enchantment.FIRE_ASPECT;
-		}
-		if(enchantnumber == 10){
-			return Enchantment.KNOCKBACK;
-		}
-		if(enchantnumber == 11){
-			return Enchantment.LOOT_BONUS_BLOCKS;
-		}
-		if(enchantnumber == 12){
-			return Enchantment.LOOT_BONUS_MOBS;
-		}
-		if(enchantnumber == 13){
-			return Enchantment.OXYGEN;
-		}
-		if(enchantnumber == 14){
-			return Enchantment.PROTECTION_ENVIRONMENTAL;
-		}
-		if(enchantnumber == 15){
-			return Enchantment.PROTECTION_EXPLOSIONS;
-		}
-		if(enchantnumber == 16){
-			return Enchantment.PROTECTION_FALL;
-		}
-		if(enchantnumber == 17){
-			return Enchantment.PROTECTION_PROJECTILE;
-		}
-		if(enchantnumber == 18){
-			return Enchantment.SILK_TOUCH;
-		}
-		if(enchantnumber == 19){
-			return Enchantment.WATER_WORKER;
-		}
-		return null; 
-		
-	}
-		
 	
 	@Override
 	public Integer switchTask() {
