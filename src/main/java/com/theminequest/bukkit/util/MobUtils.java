6e2767ba7f9573f220fabe6f05f35b638271db60
Robert Xu <robxu9@gmail.com>
2013-08-13 18:59:57 -0400

    Add additional properties method to MobUtils (mainly for Skeletons)
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/bukkit/util/MobUtils.java
+++ src/main/java/com/theminequest/bukkit/util/MobUtils.java
@@ -1,11 +1,16 @@
 package com.theminequest.bukkit.util;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.inventory.ItemStack;
 
 public class MobUtils {
 	
-	public static EntityType getEntityType(String s){
-		if (s==null)
+	public static EntityType getEntityType(String s) {
+		if (s == null)
 			return null;
 		
 		s = s.trim();
@@ -14,15 +19,27 @@ public class MobUtils {
 		if (ret == null) {
 			try {
 				ret = EntityType.valueOf(s.toUpperCase());
-			} catch (IllegalArgumentException e) {}
+			} catch (IllegalArgumentException e) {
+			}
 		}
 		if (ret == null) {
 			try {
 				ret = EntityType.fromId(Integer.parseInt(s));
-			} catch (NumberFormatException e) {}
+			} catch (NumberFormatException e) {
+			}
 		}
 		
 		return ret != null && ret.isAlive() ? ret : null;
 	}
-
+	
+	public static LivingEntity addlProps(LivingEntity e) {
+		EntityType entityType = e.getType();
+		if (entityType == EntityType.SKELETON && ((Skeleton) e).getSkeletonType().getId() == 1) {
+			((Skeleton) e).getEquipment().setItemInHand(new ItemStack(Material.STONE_SWORD));
+		} else if (entityType == EntityType.SKELETON && ((Skeleton) e).getSkeletonType().getId() == 0) {
+			((Skeleton) e).getEquipment().setItemInHand(new ItemStack(Material.BOW));
+		}
+		return e;
+	}
+	
 }
